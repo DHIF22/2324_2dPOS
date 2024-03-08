659525012051942927d76639961bc3bd1a6b5fe0
schtl <sc@htl-kaindorf.at>
2024-03-08 09:28:47 +0100

    exercise persons aka people

---
+++ _2dPOSLessions/src/_240308_FileManipulations/_04_ReadPersons.java
@@ -0,0 +1,12 @@
+package _240308_FileManipulations;
+
+public class _04_ReadPersons {
+    public static void main(String[] args) {
+        // Your turn:
+        /*
+        Read the persons.txt file into a simple
+        List of Persons.
+        person.txt -> List<Person> persons.
+         */
+    }
+}
